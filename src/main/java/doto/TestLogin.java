735690fab2402fbd5cd2e4d2abcb672756468197
Xiaowawa <wabanxian@gmail.com>
2017-12-01 17:12:05 +0800

    up up

--- src/main/java/doto/TestLogin.java
+++ src/main/java/doto/TestLogin.java
@@ -14,13 +14,14 @@ import service.LoginPage;
  */
 public class TestLogin {
     LoginPage loginPage = new LoginPage();
+    dxcsass dxcsass;
 
     public boolean gettitle() {
      return   loginPage.gettitle();
     }
 
     public void login() throws InterruptedException  {
-        loginPage.Login("18708151438", "zang1020330");
+        loginPage.Login(dxcsass.getUsrname(), dxcsass.getPassword());
     }
    public String resultmsg(){
       return   loginPage.getErr();
---
+++ src/main/java/doto/TestOrder.java
@@ -0,0 +1,10 @@
+package doto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Xiaowawa
+ * Date: 2017/12/1
+ * Time: 16:26
+ */
+public class TestOrder {
+}
